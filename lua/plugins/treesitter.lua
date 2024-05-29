8cc69fc0195b075b61059a23c74fd55e8ce4c7ed
Gabriels <gabripub@gmail.com>
2024-05-29 11:55:43 +0300

    replace comment.nvim with ts-comment in favour of native commenting in neovim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,27 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
-    "JoosepAlviste/nvim-ts-context-commentstring",
     {
-      "numToStr/Comment.nvim",
-      keys = {
-        { "gcc", mode = "n", desc = "Comment toggle current line" },
-        { "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
-        { "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
-        { "gbc", mode = "n", desc = "Comment toggle current block" },
-        { "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
-        { "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
-      },
-      opts = function()
-        return {
-          pre_hook = require(
-            "ts_context_commentstring.integrations.comment_nvim"
-          ).create_pre_hook(),
-        }
-      end,
-      config = function(_, opts)
-        require("Comment").setup(opts)
-      end,
+      "folke/ts-comments.nvim",
+      opts = {},
+      event = "VeryLazy",
+      enabled = vim.fn.has "nvim-0.10.0" == 1,
     },
   },
   build = ":TSUpdate",
